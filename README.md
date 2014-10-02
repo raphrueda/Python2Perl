96f1134e9f10d09d3fd48cbdf31d55cb328a5ebd
Raphael <raphrueda@gmail.com>
2014-10-02 20:43:32 +1000

    Implemented foreach loops. Added the secondary pass to translate lines from one line structures.

--- README.md
+++ README.md
@@ -6,10 +6,7 @@ COMP2041 Assignment 1
 	- Subset 0
 	- Subset 1
 	- Subset 2
-	    + Will take care of breaks and continues on another pass
-	    + One liner bodies have been tagged for translation in a later pass
 	- Subset 3
-	    + Multi-line if and while statements
-	    + Any missed closing braces need to be added in a later pass
+	    + Need more clarification of int()
 
  
--- py2pl.pl
+++ py2pl.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-@variables = ();
+%variables = ();
 @converted = ();
 
 $prevIndent = "";
@@ -11,7 +11,21 @@ while($line = <>){
 	#keep track of the indentation of this line
 	$currIndent = $1;
 	chomp $currIndent;
-	chomp $prevIndent;
+    }
+    if(length($currIndent) < length($prevIndent) && $line !~ /^\s*(else|elif)/){
+	$currLen = length($currIndent) / 4;
+	$prevLen = length($prevIndent) / 4;
+	@closingBraces = ();
+	while ($currLen < $prevLen){
+	    $indent = "";
+	    for (1..$currLen){
+		$indent .= "    ";
+	    }
+	    $blockClose = $indent . "}\n";
+	    push @closingBraces, $blockClose;
+	    $currLen ++;
+	}
+	push @converted, reverse(@closingBraces);
     }
     if($line =~ // && $. == 1){
 	#for the first line
@@ -21,8 +35,13 @@ while($line = <>){
 	push @converted, $line;
     } elsif($line =~ /^\s*print\s*(.*)\s*$/){
 	#print statement
-	$toPrint = changeVar($1);
-	$newLine = $currIndent . "print $toPrint, \"\\n\";\n";
+	$toPrint = $1;
+	if($toPrint ne ""){
+    	    if($toPrint =~ /^[^\"].*[^\"]$/){$toPrint = changeVar($toPrint);}
+	    $newLine = $currIndent . "print $toPrint, \"\\n\";\n";
+	} else {
+	    $newLine = $currIndent . "print \"\\n\";\n";
+	}
 	push @converted, $newLine;
     } elsif($line =~ /^\s*([a-z][a-z0-9]*)\s*=\s*(.*)\s*/){
 	#variable assignment
@@ -31,12 +50,12 @@ while($line = <>){
 	#extract variable value and look for variables inside
 	$varVal = changeVar($2);
 	#keep track of variable name
-	push @variables, $varName;
+	$variables{$varName} = $varVal;
 	#construct the new line
 	$newLine = $currIndent . "\$$varName = $varVal;\n";
 	push @converted, $newLine;
     } elsif($line =~ /^\s*(if|while|elif)\s*(.*)\s*:/){
-	#if statement and while loops
+	#conditional structures
 	#extract guard
 	$condition = changeVar($2);
 	$blockType = $1;
@@ -47,7 +66,6 @@ while($line = <>){
 	    $newLine = $currIndent . $blockType . " (" . $condition . ") {\n";
 	    push @converted, $newLine;
 	    if ($line =~ /:\s*.+\s*$/){
-		print "yolo\n";
 	        #one line if/while
 	        $line =~ /^\s*[^ ]*\s*.*:\s*(.*)\s*$/;
 	        #store the actions, separated by a ;
@@ -56,10 +74,10 @@ while($line = <>){
 	        foreach $action(@actions){
 		    #push each one with the appropriate indent
 		    #each action is untranslated, second pass will deal with these
-        	    $newAction = "TODO" . $currIndent . "    " . $action . ";\n";
+        	    $newAction = "TODO" . $currIndent . "    " . $action . "\n";
 		    push @converted, $newAction;
                 }
-	        #push a block close
+	        #push a block closer
 	        $closeBlock = $currIndent . "}\n";
 	        push @converted, $closeBlock;
 	    }
@@ -67,18 +85,103 @@ while($line = <>){
     } elsif($line =~ /^\s*else/){
 	$newLine = $currIndent . "} else {\n";
 	push @converted, $newLine;
+    } elsif($line =~ /^\s*for\s*([a-z][a-z0-9]*)\s*in range\((.*)\)/){
+	#for/range handler
+	$iterator = $1;
+	$variables{$iterator} = 1;
+	push @variables, $iterator;
+	$range = $2;
+	if($range =~ /^([a-z0-9\+\-\/\*\% ]*), ([a-z0-9\+\-\/\*\% ]*)/){
+	    $rangeLow = $1;
+	    $rangeHigh = $2;
+	} elsif($range =~ /^([a-z0-9\+\-\/\*\% ]*)$/){
+	    $rangeLow = 0;
+	    $rangeHigh = $1;
+	}
+	if($rangeHigh =~ /([\+\-])\s*([0-9]*)\s*$/){
+	    $numericDisplacement = $2 - 1 if $1 eq "+";
+	    $numericDisplacement = $2 + 1 if $1 eq "-";
+	    $rangeHigh =~ s/$2/$numericDisplacement/;
+	} else {
+	    $rangeHigh --;
+	}
+	if($rangeHigh =~ /^(.*)\s*[\+\-]\s*0/){
+	    $rangeHigh = $1;
+	    $rangeHigh =~ s/\s*$//;
+	}
+	$rangeLow = changeVar($rangeLow);
+	$rangeHigh = changeVar($rangeHigh);	
+	$newLine = $currIndent . "foreach \$$iterator ($rangeLow..$rangeHigh) {\n";
+	push @converted, $newLine;
+    } elsif($line =~ /^\s*sys\.stdout\.write\((.*)\)/){
+	$newLine = $currIndent . "print $1;\n";
+	push @converted, $newLine;
+    } elsif($line =~ /^\s*(break|continue)/){
+	$newLine = $currIndent . "last;\n" if $1 eq "break";
+	$newLine = $currIndent . "next;\n" if $1 eq "continue";
+	push @converted, $newLine;
     } else {
-	$commentedLine = "\#". $line;
-	push @converted, $commentedLine;
+	if($line =~ /^\s*import/){
+	    #ignore
+	} else {
+	    $untransLine = "TODO". $line;
+	    push @converted, $untransLine;
+	}
     }
     $prevIndent = $currIndent;
 }
 
-print @converted;
+$currIndent = "";
+if(length($currIndent) < length($prevIndent)){
+    $blockClose = $currIndent . "}\n";
+    push @converted, $blockClose;
+}
+
+@refinement = ();
+foreach $line(@converted){
+    $line =~ s/int\(sys\.stdin\.readline\(\)\)/\<STDIN\>/;
+    $line =~ s/sys\.stdin\.readline\(\)/\<STDIN\>/;
+    if($line =~ /^TODO/){ #secondary pass
+	$line = substr $line, 4;
+	if($line =~ /^(\s*)/){
+            #keep track of the indentation of this line
+            $currIndent = $1;
+            chomp $currIndent;
+    	}
+	if($line =~ /^\s*print\s*(.*)\s*$/){
+            #print statement
+            $toPrint = $1;
+            if($toPrint =~ /^[^\"].*[^\"]$/){$toPrint = changeVar($toPrint);}
+            $newLine = $currIndent . "print $toPrint, \"\\n\";\n";
+	    push @refinement, $newLine;
+	} elsif($line =~ /^\s*([a-z][a-z0-9]*)\s*=\s*(.*)\s*/){
+	    #variable assignment
+            #extract variable name
+            $varName = $1;
+            #extract variable value and look for variables inside
+            $varVal = changeVar($2);
+            #keep track of variable name
+            $variables{$varName} = $varVal;
+            #construct the new line
+            $newLine = $currIndent . "\$$varName = $varVal;\n";
+            push @refinement, $newLine;
+	} elsif($line =~ /^\s*(break|continue)/){
+	    $newLine = $currIndent . "last;\n" if $1 eq "break";
+            $newLine = $currIndent . "next;\n" if $1 eq "continue";
+            push @refinement, $newLine;
+	} else {
+	    push @refinement, $line;
+	}
+    } else {
+	push @refinement, $line;
+    }
+}
+
+print @refinement;
 
 sub changeVar {
     my ($line) = @_;
-    foreach my $var (@variables){
+    foreach my $var ( keys %variables){
 	$line =~ s/\b($var)\b/\$$var/g;
     }
     return $line;
